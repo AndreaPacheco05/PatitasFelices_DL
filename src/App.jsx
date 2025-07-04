6a88cb6353bffa1b6ccc737c60019788a6b4a1c2
Santiago Villabona <villabonasantiago@gmail.com>
2025-07-04 17:51:33 -0400

    se instaló todo el entorno de react

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,35 @@
-import React, { useContext } from 'react';
-import { Route, Routes, Navigate } from 'react-router-dom';
-import Categorias from './views/Categorias';
-import Footer from './components/Footer';
-import Header from './components/Header';
-import Navbar from './components/Navbar';
-import Home from './views/Home';
-import CrearPublicacion from './views/CrearPublicacion';
-import Detalle from './views/Detalle';
-import Perfil from './views/Perfil';
-import EditarInfo from './views/EditarInfo';
-import Registrar from './views/Registrar';
-import Tienda from './views/Tienda';
-import IniciarSesion from './views/IniciarSesion';
-import NotFound from './views/NotFound';
-import { UserContext }  from './context/UsuarioContext';
-import './assets/css/App.css';
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0)
 
-};
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
 
-export default App;
+export default App
