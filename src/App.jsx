d0db793c22bb46e429b54e57939d97a9943b5a83
Andrea Pacheco <aipachecor04@gmail.com>
2025-07-03 22:25:12 -0400

    Versión 0.1.0 (Archivos en blanco)

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import { Route, Routes, Navigate } from 'react-router-dom';
+import Categorias from './views/Categorias';
+import Footer from './components/Footer';
+import Header from './components/Header';
+import Navbar from './components/Navbar';
+import Home from './views/Home';
+import CrearPublicacion from './views/CrearPublicacion';
+import Detalle from './views/Detalle';
+import Perfil from './views/Perfil';
+import EditarInfo from './views/EditarInfo';
+import Registrar from './views/Registrar';
+import Tienda from './views/Tienda';
+import IniciarSesion from './views/IniciarSesion';
+import NotFound from './views/NotFound';
+import { UserContext }  from './context/UsuarioContext';
+import './assets/css/App.css';
