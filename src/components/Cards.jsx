c669a451427ad1b2275bcd2c47b10aaab8afca0e
Santiago Villabona <villabonasantiago@gmail.com>
2025-07-06 19:24:53 -0400

    se agregó el botón de añadir a fav

--- src/components/Cards.jsx
+++ src/components/Cards.jsx
@@ -1,20 +1,26 @@
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
+import "../assets/css/Tienda.css";
 
 
-function Cards({nombre, imagen, desc, precio, id}) {
+function Cards({ nombre, imagen, desc, precio, id }) {
+    
+    function favFunction() {
+        alert("Se guardó en favoritos")
+    }
   return (
-    <Card style={{ width: '12rem' }}>
-          <Card.Img variant="top" src={imagen} />
+    <Card style={{ width: "12rem" }}>
+      <Card.Img variant="top" src={imagen} />
       <Card.Body>
-              <Card.Title>{nombre}</Card.Title>
-        <Card.Text>
-                  precio:{precio}
-              </Card.Text>
-              <Link to={`/detalle/${id}`}>
-        <Button variant="primary">Ver más</Button>
-              </Link>
+        <Card.Title>{nombre}</Card.Title>
+              <Card.Text>precio: {precio}</Card.Text>
+              <div className='botones-container'>
+        <button className='boton-card' onClick={favFunction}>&#9829;</button>
+        <Link to={`/detalle/${id}`}>
+          <button className='boton-card'>Ver más</button>
+        </Link>   
+              </div>
       </Card.Body>
     </Card>
   );
