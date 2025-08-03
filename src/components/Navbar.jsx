4f374fe97b3a5005dca9844593fec888536cd9cc
Santiago Villabona <villabonasantiago@gmail.com>
2025-08-02 22:55:22 -0400

    se agregó el botón, de tienda y de favoritos

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,35 +2,51 @@ import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../context/UsuarioContext";
 
-import "../assets/css/Navbar.css"
+import "../assets/css/Navbar.css";
 import { Link } from "react-router-dom";
 
 function Navbar() {
   const { token, logout } = useContext(UserContext);
 
-
-  const setActiveClass = ({ isActive }) => isActive ? "btn active" : "btn";
-  
+  const setActiveClass = ({ isActive }) => (isActive ? "btn active" : "btn");
 
   return (
     <nav className="navbar">
       <div className="navbar-container">
         <div className="nav-izq">
           <h1>Patitas Felices</h1>
-          <NavLink to="/" className={setActiveClass}> Home </NavLink>
+          <NavLink to="/" className={setActiveClass}>
+            {" "}
+            Home{" "}
+          </NavLink>
+          {token && (
+            <NavLink to="/tienda" className={setActiveClass}>
+              Tienda
+            </NavLink>
+          )}
         </div>
 
         <div className="nav-der">
           {token ? (
             <>
-              <NavLink to="/perfil" className={setActiveClass}>Perfil</NavLink>
-              <button onClick={logout} className="btn">Cerrar sesión</button>
+              <NavLink to="/favoritos" className={setActiveClass}>
+                Favoritos
+              </NavLink>
+              <NavLink to="/perfil" className={setActiveClass}>
+                Perfil
+              </NavLink>
+              <button onClick={logout} className="btn">
+                Cerrar sesión
+              </button>
             </>
           ) : (
-            
             <>
-              <NavLink to="/login" className={setActiveClass}>Login</NavLink>
-              <NavLink to="/registrar" className={setActiveClass}>Registrar</NavLink>
+              <NavLink to="/login" className={setActiveClass}>
+                Login
+              </NavLink>
+              <NavLink to="/registrar" className={setActiveClass}>
+                Registrar
+              </NavLink>
             </>
           )}
         </div>
@@ -38,6 +54,5 @@ function Navbar() {
     </nav>
   );
 }
-
 
 export default Navbar;
